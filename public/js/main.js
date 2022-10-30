c3b158ccbee5ad05ba145cf90a7b26b516545221
ShortyTapki <v.v.vinogradov99@gmail.com>
2022-10-30 19:07:05 +0300

    Added all features to the main page

--- public/js/main.js
+++ public/js/main.js
@@ -24,6 +24,8 @@ const firebase_app = initializeApp(firebaseConfig);
 const db = getFirestore(firebase_app);
 
 let data = [];
+const searchBar = document.querySelector('#searchbar');
+searchBar.value = '';
 
 const getData = async () => {
   const data = [];
@@ -41,42 +43,34 @@ const defaultTmpl =
   data.reduce(
     (tmpl, { name, imgsrc, notifications, id }) =>
       tmpl +
-      `<div class="cart-item cart-grid" id="${id}">
-      <div class="cart-item__left-col">
-        <img
-          src="../assets/images/${imgsrc}"
-          alt=""
-          class="cart-item-image"
-        />
-        <p>
-          ${name}
-        </p>
-      </div>
-      <div class="cart-item__right-col">
-        <select class="item-status-select">
+      `<li class="devices-item row" id="${id}">
+        <img src="../assets/images/${imgsrc}" alt="" class="item-img" />
+        <p class="item-name">${name}</p>
+        <select class="item-status">
           <option value="Свободен">Свободен</option>
           <option value="В работе">В работе</option>
         </select>
-        <img
-          src="../assets/svg/notif_${notifications}.svg"
-          alt=""
-          class="item-notif-btn"
-          id="${id}"
-          type="${notifications}"
-        />
+        <div class="svg-container centered">
+          <img
+            src="../assets/svg/notif_${notifications}.svg"
+            alt=""
+            class="notif-btn"
+            id="${id}"
+            type=${notifications}
+          />
       </div>
-    </div>`,
+    </li>`,
     ''
   );
 
-const cart = document.querySelector('.cart-items');
-cart.innerHTML = defaultTmpl;
+const devicesList = document.querySelector('.devices');
+devicesList.insertAdjacentHTML('beforeend', defaultTmpl);
 
-document.querySelectorAll('.cart-item').forEach((item) => {
+document.querySelectorAll('.devices-item').forEach((item) => {
   const id = item.getAttribute('id');
   item.addEventListener('click', (e) => {
     e.stopImmediatePropagation();
-    openAnalytics(id);
+    // openAnalytics(id);
   });
 });
 
@@ -85,57 +79,56 @@ const handleSearch = async (e) => {
   const updateCart = (items) => {
     items.map((item) => {
       const { name, imgsrc, notifications, id } = item;
-      const tmpl = `<div class="cart-item cart-grid">
-      <div class="cart-item__left-col">
-        <img
-          src="../assets/images/${imgsrc}"
-          alt=""
-          class="cart-item-image"
-        />
-        <p>
-          ${name}
-        </p>
-      </div>
-      <div class="cart-item__right-col">
-        <select class="item-status-select">
-          <option value="Свободен">Свободен</option>
-          <option value="В работе">В работе</option>
-        </select>
-        <img
-          src="../assets/svg/notif_${notifications}.svg"
-          alt=""
-          class="item-notif-btn"
-          id="${id}"
-          type="${notifications}"
-        />
-      </div>
-    </div>`;
-      cart.insertAdjacentHTML('beforeend', tmpl);
+      const tmpl = `<li class="devices-item row" id="${id}">
+                      <img src="../assets/images/${imgsrc}" alt="" class="item-img" />
+                      <p class="item-name">${name}</p>
+                      <select class="item-status">
+                        <option value="Свободен">Свободен</option>
+                        <option value="В работе">В работе</option>
+                      </select>
+                      <div class="svg-container centered">
+                        <img
+                          src="../assets/svg/notif_${notifications}.svg"
+                          alt=""
+                          class="notif-btn"
+                          id="${id}"
+                          type="${notifications}"
+                        />
+                      </div>
+                    </li>`;
+      devicesList.insertAdjacentHTML('beforeend', tmpl);
     });
   };
-  cart.innerHTML = '';
+
+  devicesList.innerHTML = '';
   let searchResults;
   const query = e.target.value;
   if (query === '') {
-    cart.innerHTML = defaultTmpl;
+    devicesList.innerHTML = defaultTmpl;
     return;
   }
   if (parseInt(query)) {
     searchResults = data.filter(({ id }) => id === query);
-    updateCart(searchResults);
+    console.log(searchResults.length);
+    searchResults.length
+      ? updateCart(searchResults)
+      : (devicesList.innerHTML =
+          '<li class="devices-item row centered typo-1">Приборов не найдено</li>');
+
     return;
   }
   searchResults = data.filter(({ name }) =>
     name.toLowerCase().includes(query.toLowerCase())
   );
-  updateCart(searchResults);
+  searchResults.length
+    ? updateCart(searchResults)
+    : (devicesList.innerHTML =
+        '<li class="devices-item row centered typo-1">Приборов не найдено</li>');
 };
 
-document
-  .querySelector('#searchbar')
-  .addEventListener('input', (e) => handleSearch(e));
+searchBar.addEventListener('input', (e) => handleSearch(e));
 
-document.querySelectorAll('.item-notif-btn').forEach((btn) => {
+document.querySelectorAll('.notif-btn').forEach((btn) => {
   btn.addEventListener('click', async () => {
     const id = btn.getAttribute('id');
     const type = btn.getAttribute('type');
